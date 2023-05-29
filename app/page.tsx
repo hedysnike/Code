cdcf01317e04d2db3d78df921e6f0335b0133c4e
Nika <arebanikko@gmail.com>
2023-05-29 22:07:04 +0400

    checkout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from 'react'
+import { Icon } from '@iconify/react';
 
 // Prisma does not support Edge without the Data Proxy currently
 // export const runtime = 'edge'
@@ -7,13 +8,22 @@ export const dynamic = 'force-dynamic'
 
 export default function Home() {
  
- 
+  const demoCode = `import { Button } from '@mantine/core';
+
+  
+  function Demo() {
+    return <Button>Hello</Button>
+  }`;
+  
+  
  
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
+    <main className="relative flex flex-col items-center justify-center min-h-screen">
 
-
-
+      <pre className='text-white bg-[#1A1B1E]'>
+      <Icon icon="iconamoon:copy" />
+        {demoCode}
+      </pre>
 
 
     </main>
